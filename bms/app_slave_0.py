5fc3cc2c80a2f0d9f23b6807a2a761f7cd688a4e
acousticwaveprop <hwiyong.choi@avikus.ai>
2022-11-22 13:09:02 +0900

    FEAT: two's complementary number has implemented

--- bms/app_slave_0.py
+++ bms/app_slave_0.py
@@ -4,74 +4,94 @@ from random import uniform
 from pyModbusTCP import utils
 
 
-
-
 import tkinter as tk
 master = tk.Tk()
-# master.geometry("500x500")
 
-# Text Parameters
-text_width = 50
+
+
+
+
+# TK Parameters /////////////////////////////////////////////////////////////////////////////////
+
+# Display Parameters
+text_width  = 50
 text_height = 1.5
-text_bg = "#9BC2E6"
+text_bg     = "#9BC2E6"     # Background Colour
 
 # Entry Parameters
 entry_width = 30
 
+#//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+# Entry and Diplay Cells /////////////////////////////////////////////////////////////////////////////////
+
 # Signal 1
-tk.Label(master, text="Signal 1 (REQ: BMS -> NAS):").grid(row=0, column=0)
+tk.Label(master, text="Signal 1 (BMS Status in BMS -> NAS REQ Pack)").grid(row=0, column=0)
 o1 = tk.Text(master, width=text_width, height=text_height, bg = text_bg)
 o1.grid(row=0, column=1, padx=20, pady=5)
 
-tk.Label(master, text="Signal 1 (RESP: NAS -> BMS):").grid(row=0, column=2)
-e1 = tk.Entry(master, width=entry_width)
-e1.grid(row=0, column=3, padx=20, pady=5)       
+# tk.Label(master, text="Signal 1 (RESP: NAS -> BMS):").grid(row=0, column=2)
+# e1 = tk.Entry(master, width=entry_width)
+# e1.grid(row=0, column=3, padx=20, pady=5)       
+
+
 
 
 
 # Signal 2
-tk.Label(master, text="Signal 2 (REQ: BMS -> NAS):").grid(row=1, column=0)
+tk.Label(master, text="Signal 2 (RPM Set Pt in BMS -> NAS REQ Pack):").grid(row=1, column=0)
 o2 = tk.Text(master, width=text_width, height=text_height, bg = text_bg)
 o2.grid(row=1, column=1, padx=20, pady=5)
 
-tk.Label(master, text="Signal 2 (RESP: NAS -> BMS):").grid(row=1, column=2)
-e2 = tk.Entry(master, width=entry_width)
-e2.grid(row=1, column=3, padx=20, pady=5)  
+# tk.Label(master, text="Signal 2 (RESP: NAS -> BMS):").grid(row=1, column=2)
+# e2 = tk.Entry(master, width=entry_width)
+# e2.grid(row=1, column=3, padx=20, pady=5)  
+
+
+
 
 
 # Signal 3
-tk.Label(master, text="Signal 3 (REQ: BMS -> NAS):").grid(row=2, column=0)
+tk.Label(master, text="Signal 3 (RPM Actual in BMS -> NAS REQ Pack):", anchor="w").grid(row=2, column=0)
 o3 = tk.Text(master, width=text_width, height=text_height, bg = text_bg)
 o3.grid(row=2, column=1, padx=20, pady=5)
 
-tk.Label(master, text="Signal 3 (RESP: NAS -> BMS):").grid(row=2, column=2)
-e3 = tk.Entry(master, width=entry_width)
-e3.grid(row=2, column=3, padx=20, pady=5)  
+# tk.Label(master, text="Signal 3 (RESP: NAS -> BMS):").grid(row=2, column=2)
+# e3 = tk.Entry(master, width=entry_width)
+# e3.grid(row=2, column=3, padx=20, pady=5)  
+
+
 
 
 # Signal 4
 # Label
-tk.Label(master, text="Signal 4 (REQ: NAS -> BMS):").grid(row=3, column=0)
-
+tk.Label(master, text="Signal 4 (NAS Status to be included in NAS -> BMS RESP Pack):", anchor="w").grid(row=3, column=0)
 e4 = tk.Entry(master, width=entry_width)       
 e4.grid(row=3, column=1, padx=20, pady=5)
 
 
+tk.Label(master, text="[NAS-ABNORMAL, NAS-AUTO-ACT, NAS-AUTO-RDY] (Default: 001)", anchor="w").grid(row=3, column=2)
+# tk.Label(master, text="Signal 4 (RESP: BMS -> NAS):").grid(row=3, column=2)
+# o4 = tk.Text(master, width=text_width, height=text_height, bg = text_bg)
+# o4.grid(row=3, column=3, padx=20, pady=5)
 
-tk.Label(master, text="Signal 4 (RESP: BMS -> NAS):").grid(row=3, column=2)
-o4 = tk.Text(master, width=text_width, height=text_height, bg = text_bg)
-o4.grid(row=3, column=3, padx=20, pady=5)
+
 
 
 
 # Signal 5
-tk.Label(master, text="Signal 5 (REQ: NAS -> BMS):").grid(row=4, column=0)
+tk.Label(master, text="Signal 5 (RESP: NAS -> BMS):", anchor="w").grid(row=4, column=0)
 e5 = tk.Entry(master, width=entry_width)
 e5.grid(row=4, column=1, padx=20, pady=5)
 
-tk.Label(master, text="Signal 5 (RESP: BMS -> NAS):").grid(row=4, column=2)
-o5 = tk.Text(master, width=text_width, height=text_height, bg = text_bg)
-o5.grid(row=4, column=3, padx=20, pady=5)
+# tk.Label(master, text="Signal 5 (RESP: BMS -> NAS):").grid(row=4, column=2)
+# o5 = tk.Text(master, width=text_width, height=text_height, bg = text_bg)
+# o5.grid(row=4, column=3, padx=20, pady=5)
 
 
 
@@ -113,7 +133,9 @@ def stop_slave_server():
 
 
 
-# Get BMS Data -------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------
+# - Get Data from REQ Signal from BMS
+
 
 # BMS Status (Function Code 0x0F (15:Write Multiple Coils))
 # - pyModbusTCP automatically sends response to the master
@@ -158,16 +180,17 @@ def get_bms_actual_set_rpm():
 
 
     # Display to the GUI (Two's complementary Number)
-    o2.insert(tk.INSERT,"10 * RPM Setting:" + str(_to_int(hex2complement(bms_set_actual_rpm[0]),16)))
-    o3.insert(tk.INSERT,"10 * RPM Actual:"  + str(_to_int(hex2complement(bms_set_actual_rpm[1]),16)))
+    o2.insert(tk.INSERT,"RPM Setting:" + str(_to_int(hex2complement(bms_set_actual_rpm[0]),16)))
+    o3.insert(tk.INSERT,"RPM Actual:"  + str(_to_int(hex2complement(bms_set_actual_rpm[1]),16)))
     
     # Display to the terminal
-    print("RPM Setting:", int(bms_set_actual_rpm[0] / 10))
-    print("RPM Actual :", int(bms_set_actual_rpm[1] / 10))
+    print("10 X RPM Setting:", str(_to_int(hex2complement(bms_set_actual_rpm[0]), 16)))
+    print("10 X RPM Actual :", str(_to_int(hex2complement(bms_set_actual_rpm[1]), 16)))
+    # print("RPM Setting:", int(bms_set_actual_rpm[0] / 10))
+    # print("RPM Actual :", int(bms_set_actual_rpm[1] / 10))
 
     # Print for Two's complement of Hex Number
-    #print("Setting Point RPM:", bin(bms_set_actual_rpm[0]), "Twos compliment of Hex Number:", bin(bin(bms_set_actual_rpm[0]) & bin(0xffffffff)))
-    print("Setting Point RPM:", bin(bms_set_actual_rpm[0]), "Hex:", bin(int(65535)), "Two's complementary:", hex2complement(bms_set_actual_rpm[0]), "Comp-Hex-to-int:", _to_int(hex2complement(bms_set_actual_rpm[0]),16))
+    # print("Setting Point RPM:", bin(bms_set_actual_rpm[0]), "Hex:", bin(int(65535)), "Two's complementary:", hex2complement(bms_set_actual_rpm[0]), "Comp-Hex-to-int:", _to_int(hex2complement(bms_set_actual_rpm[0]),16))
 # --------------------------------------------------------------------
 
 
@@ -175,9 +198,10 @@ def get_bms_actual_set_rpm():
 
 
 
-#////////////////////////////////////////////////////////////////////
-# Bits, HEX Manipulation, Utils
-# - Ref: https://www.codegrepper.com/code-examples/python/how+to+convert+integer+to+hex+signed+2%27s+complement+in+python
+# ---------------------------------------------------------------------------------------------------
+# - Bits, HEX Manipulation, Utils
+# - References
+#  -> https://www.codegrepper.com/code-examples/python/how+to+convert+integer+to+hex+signed+2%27s+complement+in+python
 def to_hex(val, nbits):
   return hex((val + (1 << nbits)) % (1 << nbits)).lstrip('0x')
 
@@ -190,13 +214,14 @@ def _to_int(val, nbits):
     if i >= 2 ** (nbits - 1):
         i -= 2 ** nbits
     return i
-#////////////////////////////////////////////////////////////////////
+#----------------------------------------------------------------------------------------------------
 
 
 
 
 
-# HiNAS -> BMS  /////////////////////////////////////////////////////
+# ---------------------------------------------------------------------------------------------------
+# - Input Data to be sent to BMS as a RESPONSE
 
 # (Function Code 0x02 (Master Setting is "02: Read Discrete Inputs(1X)") )
 def resp_nas_status():
@@ -213,6 +238,7 @@ def resp_nas_status():
     else:
         gui_input = e4.get()
 
+    # Get bits from the entry
     nas_auto_mode_ready = int(gui_input[2]) # bit 0
     nas_auto_mode_act   = int(gui_input[1]) # bit 1
     nas_abnormal        = int(gui_input[0]) # bit 2
@@ -232,13 +258,18 @@ def resp_nas_rpm_cmd():
     else:
         gui_input = e5.get()
     
-    # Under Dev
-    # gui_input = _to_int(hex2complement(10*gui_input),16)
-    # nas_set_rpm = server.data_bank.set_input_registers(0,[gui_input])
+  
+    num_bits =16
+    rpm_set_hex2c = hex2complement(int(gui_input))
+    rpm_set_int2c = _to_int(rpm_set_hex2c,num_bits)
+    server.data_bank.set_input_registers(0,[rpm_set_int2c])
+
+    print("RPM SET PT hex2c:", rpm_set_hex2c, "int2c:", rpm_set_int2c)
+
 
     # (Deprecated)----------------------------------------------------------------
-    nas_set_rpm = server.data_bank.set_input_registers(0,[int(10*int(gui_input))])
-    print("NAS_RPM_SET(Dec):", int(10*int(gui_input)))
+    # nas_set_rpm = server.data_bank.set_input_registers(0,[int(10*int(rpm_set_int2cgui_input))])
+    # print("NAS_RPM_SET(Dec):", int(10*int(gui_input)))
     # ----------------------------------------------------------------------------
 
 # ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -280,8 +311,8 @@ def show():
 # Button ------------------------------------------------------------------------------------------------------------------------------
 tk.Button(master, text="Start Slave(Server)", width=10, command=open_slave_server).grid(row=5, column=0, sticky= "w", padx=10, pady=5)
 tk.Button(master, text="Stop Slave(Server)", width=10, command=stop_slave_server).grid(row=5, column=1, sticky= "w", padx=10, pady=5)
-tk.Button(master, text="REQ (NAS->BMS)", width=10, command=show).grid(row=5, column=2, sticky= "w", padx=10, pady=5)
-tk.Button(master, text="GET BMS STATUS", width=10, command=get_bms_set_rpm).grid(row=5, column=3, sticky= "w", padx=10, pady=5)
+tk.Button(master, text="Start Test", width=10, command=show).grid(row=5, column=2, sticky= "w", padx=10, pady=5)
+# tk.Button(master, text="GET BMS STATUS", width=10, command=get_bms_set_rpm).grid(row=5, column=3, sticky= "w", padx=10, pady=5)
 tk.Button(master, text="Exit", width=10, command=master.quit).grid(row=5, column=3, sticky="e", padx=10, pady=5)
 #--------------------------------------------------------------------------------------------------------------------------------------
 
